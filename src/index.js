ba2b236c50bce497478e6ec6d899a819eb4b85a5
Pramod Laha <pramodlaha.pm.08@gmail.com>
2025-01-07 18:30:34 +0545

    feat(server): Set up Express server with CORS and cookie parser

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 dotenv.config({
-    path: "./env",
-})
-connectDB();
+  path: "./env",
+});
+const port = process.env.PORT || 8000;
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server listening on ${port}`);
+    });
+    app.on("error", (err) => {
+      console.error(`Server error: ${err}`);
+      process.exit(1);
+    });
+  })
+  .catch((err) => {
+    console.error(`MongoDB connection error: ${err}`);
+    process.exit(1);
+  });
